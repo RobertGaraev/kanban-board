1cb76754fc3bf7b812db6641069c3319ab3b8838
Robert <rob.garaev2000@yandex.ru>
2026-03-08 23:46:24 +0300

    Initial setup: Docker, Prisma, NestJS base

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUsers() {
+    return this.prisma.user.findMany();
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
